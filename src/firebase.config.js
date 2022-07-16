d76b4f85ff72665a26a2d675807434bf7208ff1f
ihascats <s.pavlovic.rs@gmail.com>
2022-07-16 09:38:40 +0200

    Add firebase

---
+++ src/firebase.config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBoCOspn-6gJs_gngCzsOgIkLwFlGc7Rew',
+  authDomain: 'buzz3r.firebaseapp.com',
+  projectId: 'buzz3r',
+  storageBucket: 'buzz3r.appspot.com',
+  messagingSenderId: '9443323288',
+  appId: '1:9443323288:web:753e141f613c749162cfed',
+};
+
+// eslint-disable-next-line no-unused-vars
+const app = initializeApp(firebaseConfig);
