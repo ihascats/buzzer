aeaf91417203984b036db8bd42f2919c412970f3
ihascats <s.pavlovic.rs@gmail.com>
2022-07-31 08:21:20 +0200

    Resize text area depending on the amount of text

--- src/components/NewBuzz.js
+++ src/components/NewBuzz.js
@@ -3,6 +3,22 @@ import './styles/NewBuzz.css';
 
 export default function NewBuzz({ user }) {
   const iconPack = Icons();
+  window.onload = () => {
+    // code from https://stackoverflow.com/questions/454202/creating-a-textarea-with-auto-resize
+    const tx = document.getElementsByTagName('textarea');
+    for (let i = 0; i < tx.length; i++) {
+      tx[i].setAttribute(
+        'style',
+        'height:' + tx[i].scrollHeight + 'px;overflow-y:hidden;',
+      );
+      tx[i].addEventListener('input', OnInput, false);
+    }
+
+    function OnInput() {
+      this.style.height = 'auto';
+      this.style.height = this.scrollHeight - 20 + 'px';
+    }
+  };
 
   return (
     <div className="newBuzzElement">
